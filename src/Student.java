2d75dd37ffb264c042c2892684837268a88aa061
Jian Feng Choo <Jian Feng Choo@LAPTOP-2LOUHK9S>
2022-10-14 15:00:30 +0800

    First commit

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Choo Jian Feng");
+		System.out.println("My matric number is 215089");
+		System.out.println("Now my address is Kolej Canselor");
+	}
+
+}
